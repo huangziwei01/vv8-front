5c64ef2904ba9b33dc624bc74398bcf47f5f3d87
vv8 <yeee444@163.com>
2023-09-14 00:52:24 +0800

    修改代理配置

--- src/api/proxy.js
+++ src/api/proxy.js
@@ -1,14 +1,15 @@
 // 该服务为 vercel serve跨域处理
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
+console.log('proxy---------------------')
 module.exports = (req, res) => {
   let target = ''
-
+  console.log(req.url, '---------')
   // 代理目标地址
   // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
   if (req.url.startsWith('/api-prod')) {
     // 这里填目标地址
-    target = 'https://api.imooc-front.lgdsunday.club'
+    target = 'https://api.imooc-front.lgdsunday.club/'
   }
   // 创建代理对象并转发请求
   createProxyMiddleware({
@@ -17,7 +18,7 @@ module.exports = (req, res) => {
     pathRewrite: {
       // 通过路径重写，去除请求路径中的 `/api-prod`
       // 例如 /api/user/login 将被转发到 http://target/user/login
-      '^/api-prod/': '/api'
+      // '^/api-prod/': '/api'
     }
   })(req, res)
 }
