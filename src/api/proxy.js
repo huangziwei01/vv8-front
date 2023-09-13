a06a0828674b51bc53adb0b8cc100f98bed99c33
vv8 <yeee444@163.com>
2023-09-13 22:39:02 +0800

    2

--- src/api/proxy.js
+++ src/api/proxy.js
@@ -6,18 +6,18 @@ module.exports = (req, res) => {
 
   // 代理目标地址
   // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-  if (req.url.startsWith('/api')) {
+  if (req.url.startsWith('/api-prod')) {
     // 这里填目标地址
-    target = 'https://api.imooc-front.lgdsunday.club'
+    target = 'https://api.imooc-front.lgdsunday.club/api'
   }
   // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
-    changeOrigin: true,
-    pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/api`
-      // 例如 /api/user/login 将被转发到 http://target/user/login
-      '^/api/': '/'
-    }
+    changeOrigin: true
+    // pathRewrite: {
+    //   通过路径重写，去除请求路径中的 `/api`
+    //   例如 /api/user/login 将被转发到 http://target/user/login
+    //   '^/api/': '/'
+    // }
   })(req, res)
 }
--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
     proxy: {
       // 代理所有 /api 的请求，该求情将被代理到 target 中
       // 开发环境
-      '/api-dev': {
+      '/api': {
         // 代理请求之后的请求地址
         target: 'https://api.imooc-front.lgdsunday.club',
         // 跨域
